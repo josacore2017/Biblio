3a4c98815c211853c02eec61ff08bf8755c67b13
Josimar Condori <santiago.condori2017@gmail.com>
2018-11-15 09:04:03 -0400

    Initial commit

---
+++ app/src/main/java/com/josacore/biblio/BookFormActivity.java
@@ -0,0 +1,13 @@
+package com.josacore.biblio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_form);
+    }
+}
